8112815c2ef0d97cee6bd77797e4441e68bc5536
1sixth <1sixth@azc.moe>
2026-01-18 00:24:55 +0800

    switch from nixos-unstable-small to nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     sops-nix = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:Mic92/sops-nix";
